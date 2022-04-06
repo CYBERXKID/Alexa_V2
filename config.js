7f613ab32af536524da290b282620382ca760949
CYBERXKID <102310990+CYBERXKID@users.noreply.github.com>
2022-04-06 20:17:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ module.exports = {
     KICKMEMSG: process.env.KICKME_MESSAGE === undefined ? 'default' : process.env.KICKME_MESSAGE,
     BLOCKCHAT: process.env.BLOCK_CHAT === undefined ? false : process.env.BLOCK_CHAT,
     OWNER: process.env.OWNER_NAME === undefined ? 'default' : process.env.OWNER_NAME,
-    ALL: process.env.ALL_CAPTION === undefined ? 'Made By Maalutty' : process.env.ALL_CAPTION,
+    ALL: process.env.ALL_CAPTION === undefined ? 'Made By Alexa' : process.env.ALL_CAPTION,
     MENTION: process.env.TAG_REPLY === undefined ? '917025868709@s.whatsapp.net' : process.env.TAG_REPLY,
     ADDMSG: process.env.ADD_MESSAGE === undefined ? 'default' : process.env.ADD_MESSAGE,
     PLKS: process.env.THERI_LIST === undefined ? false : process.env.THERI_LIST,
